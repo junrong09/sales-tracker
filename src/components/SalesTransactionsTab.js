a843ac77d455d0d12577f7edba19155eac94bf57
junrong09 <jun_rong5@hotmail.com>
2019-06-24 11:23:46 +0800

    Add react table library to display transactions

--- src/components/SalesTransactionsTab.js
+++ src/components/SalesTransactionsTab.js
@@ -1,11 +1,40 @@
 import React from "react";
+import {Column, Table} from "react-virtualized";
+import 'react-virtualized/styles.css';
 
 class SalesTransactionsTab extends React.Component {
     render() {
+        let isPropDefined = false;
+        if (typeof this.props.data.transactions !== "undefined") {
+            isPropDefined = true;
+            this.props.data.transactions.forEach(function(tran) {
+                tran.time = tran.hour.toString() + ":" + tran.min.toString();
+            });
+        } else {
+
+        }
+
         return (
             <React.Fragment>
-                <p className="b">10 Aug 2018 : Sales Transaction</p>
-
+                <p className="b sans-serif mid-gray">10 Aug 2018 : Sales Transactions</p>
+                {isPropDefined &&
+                <Table
+                    width={300}
+                    height={300}
+                    headerHeight={30}
+                    rowHeight={25}
+                    rowCount={this.props.data.transactions.length}
+                    rowGetter={({index}) => this.props.data.transactions[index]}
+                    className=""
+                    headerClassName="mid-gray"
+                    rowClassName="bb b--moon-gray"
+                    gridClassName="mid-gray"
+                >
+                    <Column width={200} label='SKU' dataKey='sku'/>
+                    <Column width={200} label='Value' dataKey='value'/>
+                    <Column width={200} label='Time' dataKey='time'/>
+                </Table>
+                }
 
             </React.Fragment>
         )
