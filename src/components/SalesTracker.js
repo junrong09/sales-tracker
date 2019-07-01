31251eaa0f53a65a6554a50055c7172c9da2dd58
junrong09 <jun_rong5@hotmail.com>
2019-07-01 15:19:37 +0800

    Modify API calls

--- src/components/SalesTracker.js
+++ src/components/SalesTracker.js
@@ -3,13 +3,11 @@ import TabBar from "./TabBar";
 import SalesOverallTab from "./SalesOverallTab";
 import SalesTransactionsTab from "./SalesTransactionsTab";
 import SalesTargetSetterTab from "./SalesTargetSetterTab";
-import {HOST} from "./Constant";
 
 class SalesTracker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: {},
             // TODO: Set back to overall
             tab: "overall"
         }
@@ -17,30 +15,20 @@ class SalesTracker extends React.Component {
 
     onTabChange = (newTab) => {
         this.setState({tab: newTab});
-        this.fetchUserProfile();
-    };
-
-    fetchUserProfile = () => {
-        fetch(HOST + "salesprofile?user=" + this.props.id, {
-            method: 'get'})
-            .then(raw  => raw.json())
-            .then(data => {
-                this.setState({data: data});
-            })
-            .catch(console.log);
+        this.props.onTransactionsFetch();
     };
 
     componentDidMount() {
-        this.fetchUserProfile();
+        this.props.onTransactionsFetch();
     }
 
     render() {
         return (
             <div className="flex flex-column items-center vh-75 w-100 mw6">
                 <TabBar onTabChange={this.onTabChange} tab={this.state.tab}/>
-                {this.state.tab === "overall" && <SalesOverallTab data={this.state.data}/>}
-                {this.state.tab === "transactions" && <SalesTransactionsTab data={this.state.data}/>}
-                {this.state.tab === "targetSetter" && <SalesTargetSetterTab data={this.state.data} fetchUserProfile={this.fetchUserProfile}/>}
+                {this.state.tab === "overall" && <SalesOverallTab data={this.props.transactions}/>}
+                {this.state.tab === "transactions" && <SalesTransactionsTab data={this.props.transactions}/>}
+                {this.state.tab === "targetSetter" && <SalesTargetSetterTab data={this.props.transactions} id={this.props.id}/>}
             </div>
         )
     }
