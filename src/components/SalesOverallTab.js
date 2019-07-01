eabb55af951c6ec8394985391ad143161634250d
junrong09 <jun_rong5@hotmail.com>
2019-07-01 15:17:35 +0800

    Add aggregate calculation

--- src/components/SalesOverallTab.js
+++ src/components/SalesOverallTab.js
@@ -12,22 +12,51 @@ import {
 } from "recharts";
 
 class SalesOverallTab extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sales: undefined,
+            target: undefined,
+            transCount: undefined,
+            memberServed: undefined
+        }
+    }
 
     roundToHundreds = (num) => {
         return Math.ceil(num / 100) * 100;
     };
 
-    render() {
-        // var transactionCount = typeof this.props.data.transactionCount === "undefined" ?
-        //     0 : this.props.data.transactionCount;
-        // var memberCount = typeof this.props.data.memberCount === "undefined" ?
-        //     0 : this.props.data.memberCount;
+    computeAggregate = () => {
+        let data = this.props.data;
+        if (typeof data === "undefined") {
+            console.log("Compute Aggregate : undefined");
+            return;
+        }
+        console.log("Computed Aggregate");
 
+        let sales = data.map(obj => obj.value).reduce((acc, val) => acc+val);
+        this.setState({sales: sales});
+        let transCount = (new Set(data.map(obj => obj.transaction_id))).size;
+        this.setState({transCount: transCount});
+        let memberServed = (new Set(data.map(obj => obj.member_id))).size;
+        this.setState({memberServed: memberServed});
+    };
+
+    componentDidMount() {
+        this.computeAggregate();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.data !== this.props.data)
+            this.computeAggregate();
+    }
+
+    render() {
         return (
             <React.Fragment>
                 <p className="b sans-serif mid-gray">24 Jun 2019 : Sales Chart</p>
                 <ResponsiveContainer width="90%" height="60%" className="mt2">
-                    <ComposedChart data={[this.props.data]}>
+                    <ComposedChart data={[this.state]}>
                         <CartesianGrid stroke="#f5f5f5"/>
                         <YAxis domain={[0, (num) => this.roundToHundreds(num * 1.1)]}>
                             <Label value="Sales Amount" angle={270} position="insideLeft"/>
@@ -37,15 +66,17 @@ class SalesOverallTab extends React.Component {
                         <Bar dataKey="target" fill="#82ca9d" maxBarSize={70}>
                             <LabelList dataKey="target" position="top"/>
                         </Bar>
-                        <Bar dataKey="actual" fill="#413ea0" maxBarSize={70}>
+                        <Bar dataKey="sales" fill="#413ea0" maxBarSize={70}>
                             <LabelList dataKey="actual" position="top"/>
                         </Bar>
                     </ComposedChart>
                 </ResponsiveContainer>
                 <p className="b sans-serif mid-gray f5 mt3">Transactions Completed:
-                    {this.props.data.transactionCount}</p>
+                    {this.state.transCount}
+                </p>
                 <p className="b sans-serif mid-gray f5 ma0">Members Served:
-                    {this.props.data.memberCount}</p>
+                    {this.state.memberServed}
+                </p>
             </React.Fragment>
         )
     }
