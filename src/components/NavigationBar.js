1add60532f6027770a9db044cc9fccd174b83af6
junrong09 <jun_rong5@hotmail.com>
2019-08-02 13:55:13 +0800

    Add display user name instead of id

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -6,7 +6,7 @@ class NavigationBar extends React.Component {
     render() {
         return (
             <div className="flex items-center w-100 ph3 justify-between justify-end-ns">
-                <p className="sans-serif mid-gray b mr3">Welcome, {this.props.id}</p>
+                <p className="sans-serif mid-gray b mr3">Welcome, {this.props.userName === null ? this.props.id : this.props.userName}</p>
                 <Link to="/">
                     <FormButton label="Logout" onClick={this.props.onLogout}/>
                 </Link>
